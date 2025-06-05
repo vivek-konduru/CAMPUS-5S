988cd71318038abbe9163355e20c089f6a5c201d
vivek-konduru <konduruvivekvardhan@gmail.com>
2025-06-06 03:38:58 +0530

    Update wsgi.py

--- fiveSManagement/wsgi.py
+++ fiveSManagement/wsgi.py
@@ -13,4 +13,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fiveSManagement.settings')
 
+django.setup()
+from django.core.management import call_command
+try:
+    call_command('makemigrations')
+    call_command('migrate')
+except Exception as e:
+    print(f"Migration error: {e}")
+
+
 application = get_wsgi_application()
